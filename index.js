deb01e63e9e7533eb8f68e64398475652590b790
kristiina kumila <kristiinakumila@gmail.com>
2021-04-22 12:11:01 +0300

    portin määrittely vaihdettu

--- index.js
+++ index.js
@@ -90,6 +90,6 @@ app.get('/howmany/diet/:number/:specialdiet', async (request, response) => {
     response.json(lopullinenLista)
 })
 
-const port = process.env.PORT || 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)

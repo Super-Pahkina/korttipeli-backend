1d3790537c44101d447688138f98be04df7a3043
kristiina kumila <kristiinakumila@gmail.com>
2021-04-22 12:16:19 +0300

    port muutettu

--- index.js
+++ index.js
@@ -91,5 +91,6 @@ app.get('/howmany/diet/:number/:specialdiet', async (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
